0b5f4790a0f545de7f8f6809f76acf34142bd245
shivam mole <shivmole1713@gmail.com>
2024-09-06 16:27:21 +0530

    added main folder to git

--- src/main/java/com/bridgelabz/dailypractice_5/practice.java
+++ src/main/java/com/bridgelabz/dailypractice_5/practice.java
@@ -1,2 +1,9 @@
-package com.bridgelabz.dailypractice_5;public class practice {
+package com.bridgelabz.dailypractice_5;
+
+public class practice {
+    public static  void main(String args[])
+    {
+        System.out.println("ading file jto git ");
+        System.out.println("wow nice to see you hopw are you dear");
+    }
 }
